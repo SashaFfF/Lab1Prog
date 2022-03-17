8ac80dfe62188c07c948cb711ea8f62a5b528ee0
SashaFfF <alexandra.filipenya3@gmail.com>
2022-03-17 11:31:44 +0300

    lab1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _153501_Filipenya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int result = firstNumber / secondNumber;
+            Console.WriteLine($"Division result: {result}");
+        }
+    }
+}
